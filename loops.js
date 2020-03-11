3e7a14e20a7b223c76c5a3a91aae8eb9ae01d047
sebastian <sverdu6@gmail.com>
2020-03-11 23:53:23 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,28 +1,36 @@
-ffunction forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push("I am 1 strange loop")
-    } else {
-      array.push(`I am ${i} strange loops.`)
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    if (i === 1){
+      array[i] = "I am 1 strange loop."
     }
+    else{
+      array[i] = `I am ${i} strange loops.`
     }
-    return array
   }
-
-function whileLoop(number) {
-  while (number > 0){
-    console.log(number--)
-  }
-  return "done"
-}
-
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-
-function doWhileLoop(array) {
-  do {
-    array = array.slice(1)
-  } while (array.length > 0 && maybeTrue())
+  console.log(array)
   return array
+}
+
+var dummy = []
+
+forLoop(dummy)
+
+function whileLoop(number){
+  while(number > 0){
+    console.log(number)
+    number = number - 1
+  }
+  return 'done'
+}
+
+var i = 0
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(integer){
+  do{
+    console.log("I run once regardless.")
+  } while (incrementVariable() <= integer)
 }
