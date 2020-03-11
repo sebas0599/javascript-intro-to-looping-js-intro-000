482537bf7f3c3685459a81844aa1d8380bbcbecd
sebastian <sverdu6@gmail.com>
2020-03-11 23:54:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,14 +1,12 @@
-function forLoop(array){
-  for (let i = 0; i < 25; i++){
-    if (i === 1){
-      array[i] = "I am 1 strange loop."
-    }
-    else{
-      array[i] = `I am ${i} strange loops.`
-    }
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
   }
-  console.log(array)
-  return array
+  return array;
 }
 
 var dummy = []
